471b5f542c230c3f884fad56c34312d8d3e364fb
John Maddock <jmmaddock@gmail.com>
2018-12-07 11:14:57 -0700

    stuf and what nots

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
-import React from 'react';
-import { Button, View, Text, Image, ImageBackground } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
+import React from "react"
+import { Button, View, Text, Image, ImageBackground } from "react-native"
+import { createStackNavigator, createAppContainer } from "react-navigation" // Version can be specified in package.json
+import AnimatedSnow from "./AnimatedSnow"
 
 class HomeScreen extends React.Component {
   render() {
-    return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Cold" onPress={() => this.props.navigation.navigate("Cold")} />
-      <Button title="Hot" onPress={() => this.props.navigation.navigate("Hot")} />
-    </View>;
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Cold"
+          onPress={() => this.props.navigation.navigate("Cold")}
+        />
+        <Button
+          title="Hot"
+          onPress={() => this.props.navigation.navigate("Hot")}
+        />
+      </View>
+    )
   }
 }
 
 class FrostyScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ImageBackground style={{ alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%' }} source={{ uri: 'http://placehold.it/600x900' }}>
-          <Image style={{ width: '10%', height: '10%' }} source={{ uri: "https://pngimage.net/wp-content/uploads/2018/06/small-image-png-3.png" }}>
-          </Image>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ImageBackground
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            height: "100%"
+          }}
+          source={{
+            uri:
+              "https://www.hdiphonewalls.com/wp-content/uploads/iPhoneX/Nature/Static%20snow%20iPhone%20X%20Wallpapers.jpg"
+          }}
+        >
+          <AnimatedSnow style={styles.snow} />
         </ImageBackground>
-
-
       </View>
-    );
+    )
   }
 }
 
 class FireyScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>You are cold</Text>
 
         {/* Look here! We "push" the Details route */}
-
       </View>
-    );
+    )
   }
 }
 
@@ -44,17 +60,25 @@ const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
     Cold: FireyScreen,
-    Hot: FrostyScreen,
+    Hot: FrostyScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: "Home"
   }
-);
+)
 
-const AppContainer = createAppContainer(RootStack);
+const AppContainer = createAppContainer(RootStack)
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return <AppContainer />
+  }
+}
+const styles = {
+  snow: {
+    backgroundColor: "#FFFFFF",
+    width: 100,
+    height: 100,
+    borderRadius: 50
   }
 }
