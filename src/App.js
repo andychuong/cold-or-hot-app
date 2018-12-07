1e7b70463c834f07eded9ba5edd7043ec30de303
Andy Chuong <achuong55@gmail.com>
2018-12-07 10:15:05 -0700

    navigation with background image

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { Button, View, Text, Image, ImageBackground } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+class HomeScreen extends React.Component {
+  render() {
+    return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button title="Cold" onPress={() => this.props.navigation.navigate("Cold")} />
+      <Button title="Hot" onPress={() => this.props.navigation.navigate("Hot")} />
+    </View>;
+  }
+}
 
-
-type Props = {};
-export default class App extends Component<Props> {
+class FrostyScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ImageBackground style={{ alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%' }} source={{ uri: 'http://placehold.it/600x900' }}>
+          <Image style={{ width: '10%', height: '10%' }} source={{ uri: "https://pngimage.net/wp-content/uploads/2018/06/small-image-png-3.png" }}>
+          </Image>
+        </ImageBackground>
+
+
       </View>
     );
   }
 }
 
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class FireyScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>You are cold</Text>
+
+        {/* Look here! We "push" the Details route */}
+
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Cold: FireyScreen,
+    Hot: FrostyScreen,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-};
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
